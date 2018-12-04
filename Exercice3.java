05914a708c44656ffe0a0ae362edd48915c5f9a5
ThibaultCougrand <thibault.cougrand@gmail.com>
2018-12-04 16:46:37 +0100

    création du scanner pour récupérer la table à calculer, mise en place de la boucle qui affiche les tables.

--- Exercice3.java
+++ Exercice3.java
@@ -1,8 +1,14 @@
 package Exercices;
 
+import java.util.Scanner;
+
 public class Exercice3 {
 
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		int table = scan.nextInt();
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(table + " fois " + i + " est égale à " + i * table);
+		}
 	}
 }
