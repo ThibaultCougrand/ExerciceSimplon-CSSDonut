d14598e87a2142c17317ab1c479a8fc8de20d123
ThibaultCougrand <thibault.cougrand@gmail.com>
2018-12-07 16:45:18 +0100

    Ajout de l'exercice11 à mon répo

--- Exercice11.java
+++ Exercice11.java
@@ -3,7 +3,10 @@ package Exercices;
 public class Exercice11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double a = 4;
+		double b = 3;
+		double result = a/b;
+		System.out.println(result);
 
 	}
 
