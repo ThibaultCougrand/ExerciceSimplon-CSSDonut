698acf0f4f43dfadfe9cca55c8579d3dbd98c5ac
ThibaultCougrand <thibault.cougrand@gmail.com>
2018-12-12 15:26:51 +0100

    Ajout des commentaires exercice13 et de l'exercice14

--- Exercice14.java
+++ Exercice14.java
@@ -1,10 +1,22 @@
 package Exercices;
 
+import java.util.Scanner;
+
 public class Exercice14 {
-
+	/**
+	 * Exercice 14
+	 * 1.demander à un utilisateur de saisir une chaine de caractère
+	 * 2.retournez cette chaine en majuscule
+	 * 3.retournez cette chaine avec la première lettre en majuscule
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un mot ou une phrase");
+		String saisi = sc.nextLine();
+		System.out.println(saisi.toUpperCase());
+		System.out.println(saisi.substring(0, 1).toUpperCase() + saisi.substring(1));
+		sc.close();
 	}
 
 }
